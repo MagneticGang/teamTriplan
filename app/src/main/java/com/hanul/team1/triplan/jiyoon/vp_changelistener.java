9dd5ed8635fa6be405961f314d5045975a008ced
jiyoonoh <njypalette@gmail.com>
2018-11-15 19:49:14 +0900

    지윤. 메인 로그인 뷰 인디케이터, 슬라이드 처리 완료

---
+++ app/src/main/java/com/hanul/team1/triplan/jiyoon/vp_changelistener.java
@@ -0,0 +1,33 @@
+package com.example.user.viewpager;
+
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+
+public class vp_changelistener implements ViewPager.OnPageChangeListener {
+
+    private ImageView[] imageViews;
+
+    public vp_changelistener(ImageView[] imageViews) {
+        super();
+        this.imageViews = imageViews;
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+    }
+
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+        for (int i = 0; i < imageViews.length; i++) {
+            imageViews[position].setBackgroundResource(R.mipmap.circle_selection);
+            if (position != i) {
+                imageViews[i].setBackgroundResource(R.mipmap.circle);
+            }
+        }
+    }
+}
